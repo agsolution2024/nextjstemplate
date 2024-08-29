48ab39369f73e968de61fe4ac7edccb4bd5656b7
Paul John Villalobos <PaulJohn.Villalobos@robinsonsretail.com.ph>
2024-08-29 09:45:45 +0800

    commit changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  distDir: "build",
-};
+// const nextConfig = {
+//   distDir: "build",
+// };
+
+const nextConfig = {};
 
 export default nextConfig;
